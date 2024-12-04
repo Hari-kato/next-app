9f2bd75fb3bac3eed26a7889143c5c3a660784e7
Hari <hari.s@katomaran.com>
2024-12-04 19:19:21 +0530

    project init

---
+++ app/auth/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center min-h-screen font-thin'>
+        <h1>Auth Screen</h1>
+    </div>
+  )
+}
+
+export default page
